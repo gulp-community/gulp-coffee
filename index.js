dd916c2f461ef28bd0af36d98c7902add4235749
Contra <contra@maricopa.edu>
2013-11-26 17:42:36 -0700

    remove clone

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 var es = require('event-stream'),
-  clone = require('clone'),
-  path = require('path'),
   coffee = require('coffee-script'),
   util = require('gulp-util');
 
 module.exports = function(opt){
-  // clone options
-  opt = opt ? clone(opt) : {};
-
   function modifyFile(file, cb){
-    var newFile = clone(file);
-
-    newFile.path = util.replaceExtension(newFile.path, ".js");
-    newFile.shortened = util.replaceExtension(newFile.shortened, ".js");
-    newFile.contents = coffee.compile(String(newFile.contents), opt);
-    cb(null, newFile);
+    file.path = util.replaceExtension(file.path, ".js");
+    file.shortened = util.replaceExtension(file.shortened, ".js");
+    file.contents = coffee.compile(String(file.contents), opt);
+    cb(null, file);
   }
 
   return es.map(modifyFile);
