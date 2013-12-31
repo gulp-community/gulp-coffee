91812f386b0b91f6f3cf02d03de82b5d500beb36
Contra <contra@maricopa.edu>
2013-12-30 20:51:34 -0700

    update deps

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ var Buffer = require('buffer').Buffer;
 
 module.exports = function(opt){
   function modifyFile(file, cb){
+    if (file.isNull()) return cb(null, file); // pass along
+    if (file.isStream()) return cb(new Error("gulp-less: Streaming not supported"));
+
+    var str = file.contents.toString('utf8');
+
     try {
-      file.contents = new Buffer(coffee.compile(String(file.contents), opt));
+      file.contents = new Buffer(coffee.compile(str, opt));
     } catch (err) {
       var newError = formatError(file, err);
       return cb(newError);
