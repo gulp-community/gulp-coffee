3e36c1e8ee5840bf072dd3d257742a9d8fa3459a
Contra <contra@maricopa.edu>
2014-06-02 12:55:47 -0700

    clean up options logic

--- index.js
+++ index.js
@@ -4,33 +4,27 @@ var gutil = require('gulp-util');
 var Buffer = require('buffer').Buffer;
 var applySourceMap = require('vinyl-sourcemaps-apply');
 var path = require('path');
+var merge = require('merge');
 
 module.exports = function (opt) {
   function modifyFile(file) {
     if (file.isNull()) return this.emit('data', file); // pass along
-    if (file.isStream()) return this.emit('error', new Error("gulp-coffee: Streaming not supported"));
+    if (file.isStream()) return this.emit('error', new Error('gulp-coffee: Streaming not supported'));
 
     var data;
     var str = file.contents.toString('utf8');
-    var dest = gutil.replaceExtension(file.path, ".js");
+    var dest = gutil.replaceExtension(file.path, '.js');
 
-    var options = {
-      literate: /\.(litcoffee|coffee\.md)$/.test(file.path)
-    };
-
-    // TODO: this is horrible, figure out a better way to copy these options over
-    if (opt) {
-      options = {
-        bare: opt.bare != null ? !! opt.bare : false,
-        header: opt.header != null ? !! opt.header : false,
-        literate: opt.literate != null ? !! opt.literate : options.literate,
-        filename: file.path,
-        sourceFiles: [path.basename(file.path)],
-        generatedFile: path.basename(dest)
-      };
-    }
-
-    if (file.sourceMap) options.sourceMap = true;
+    var options = merge({
+      bare: false,
+      header: false,
+      sourceMap: !!file.sourceMap,
+      sourceRoot: false,
+      literate: /\.(litcoffee|coffee\.md)$/.test(file.path),
+      filename: file.path,
+      sourceFiles: [path.basename(file.path)],
+      generatedFile: path.basename(dest)
+    }, opt);
 
     try {
       data = coffee.compile(str, options);
