a6facf8126f0da4960443a35009682d1c4600edd
svsool <svsool@bk.ru>
2014-06-27 04:51:47 +0400

    refactoring try/catch block

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var merge = require('merge');
 
 module.exports = function (opt) {
   function transform(file, enc, cb) {
-    if (file.isNull()) cb(null, file); // pass along
+    if (file.isNull()) cb(null, file); 
     if (file.isStream()) return cb(new PluginError('gulp-coffee', 'Streaming not supported'));
 
     var data;
@@ -29,20 +29,19 @@ module.exports = function (opt) {
 
     try {
       data = coffee.compile(str, options);
-
-      if (data.v3SourceMap && file.sourceMap) {
-        applySourceMap(file, data.v3SourceMap);
-        file.contents = new Buffer(data.js);
-      } else {
-        file.contents = new Buffer(data);
-      }
-
-      file.path = dest;
-      cb(null, file);
-
     } catch (err) {
-      cb(new PluginError('gulp-coffee', err));
+      return cb(new PluginError('gulp-coffee', err));
     }
+
+    if (data && data.v3SourceMap && file.sourceMap) {
+      applySourceMap(file, data.v3SourceMap);
+      file.contents = new Buffer(data.js);
+    } else {
+      file.contents = new Buffer(data);
+    }
+
+    file.path = dest;
+    cb(null, file);
   }
 
   return through.obj(transform);
